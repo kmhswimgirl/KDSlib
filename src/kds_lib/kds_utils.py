58418a6d0a8fc759d790afe08610e39e6f39416f
kmhswimgirl <163170240+kmhswimgirl@users.noreply.github.com>
2025-06-21 15:19:42 -0400

    fixed imports again

--- src/kds_lib/kds_utils.py
+++ src/kds_lib/kds_utils.py
@@ -1,4 +1,3 @@
-
 import serial
 from typing import Optional
 
--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,7 @@
-from KDSlib import Legato110, KdsUtil
+from src.kds_lib import Legato110, KdsUtil
 
 # test connection
 u = KdsUtil("port", 115200)
-l = Legato110()
+l = Legato110("port", 115200)
 
 # test run commands
